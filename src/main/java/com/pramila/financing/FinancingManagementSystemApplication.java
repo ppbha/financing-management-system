d5ec24652cc4405b5c0cfff678317e81ef715e97
Pramila B <ppbha811@gmail.com>
2026-08-25 11:48:37 +0300

    Initial project setup

---
+++ src/main/java/com/pramila/financing/FinancingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pramila.financing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancingManagementSystemApplication.class, args);
+	}
+
+}
